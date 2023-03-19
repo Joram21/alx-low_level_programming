9015ee6b9aaceac78e6f61a91997adbd33880b19
Joram21 <oloojoram@gmail.com>
2023-03-19 14:07:32 -0700

    task-10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: a program that prints all possible different combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int d, p;
+	for (d = 0; d < 9; p++)
+	{
+		for (p = d = 1; p <= 9; p++)
+		{
+			putchar((d % 10) + '0');
+			putchar((p % 10) + '0');
+			if (d == 8 && p == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
