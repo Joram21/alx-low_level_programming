aae65287123f48943fcebc1bceefc1bb16a2dd4f
Joram21 <oloojoram@gmail.com>
2023-03-21 13:26:23 -0700

    task-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * Description: adds two integers and returns the result
+ *
+ * Return: addition solution
+ */
+int add(int j, int k)
+{
+	int x;
+	x = j + k;
+	return (i);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**Description: Determines if the input number is greater, equal, or less than zero
+ *
+ * @c: the input number as an integer
+ *
+ * Return: 1 if greater than zero, 0 if its zero, -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
